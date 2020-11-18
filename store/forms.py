369ebdaa2456e1b74431f5b179aa04fa3e7f5a3f
BAAZIZ KHADIDJA <baazize-khadija@hotmail.com>
2020-11-18 17:32:09 +0100

    version avec un forms.py amélioré sans traitement d'erreur

--- store/forms.py
+++ store/forms.py
@@ -1,10 +1,18 @@
-from django import forms
 
-class ContactForm(forms.Form):
-    name = forms.CharField(label='Nom',
-           max_length=100,
-           widget = forms.TextInput(attrs={'class': 'form-control'}),
-           required=True)
-    email = forms.EmailField(label='Email',
-                           widget=forms.EmailInput(attrs={'class': 'form-control'}),
-                           required=True)
+from django.forms import ModelForm, TextInput, EmailInput
+from django.forms.utils import ErrorList
+from .models import Contact
+
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = ["name", "email"]
+        widget = {
+            "name" : TextInput(attrs={'class': 'form-control'}),
+            "email": EmailInput(attrs={'class': 'form-control'}),
+        }
+
+
+
+
